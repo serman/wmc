c46156d003510c755d6ac1febeeb6af870a2d9ca
Sergio <sergio@tinkerista.com>
2016-09-20 02:33:36 +0200

    framerate analytics

--- src/analytics.h
+++ src/analytics.h
@@ -23,15 +23,21 @@ public:
         faceShotPeriod++;
     }
     void update(){
-        if(ofGetElapsedTimeMillis()>(timePeriodInit+period)){
+        frameRateSum+=ofGetFrameRate();
+        frameRateCount++;
+        if(ofGetElapsedTimeMillis()>(timePeriodInit+period) ){
             faceDetectionsTotal+=faceDetectionsPeriod;
             faceShotTotal += faceShotPeriod;
+            
+            if(frameRateCount>0)
+                frameRateMean=frameRateSum/frameRateCount;
             ofLogNotice() << getString() << endl;
             timePeriodInit=ofGetElapsedTimeMillis();
    //         myTextFile.open("text.txt",ofFile::WriteOnly);
             
             ofHttpResponse resp = ofLoadURL("http://emoncms.org/input/post?apikey=d67a577b4ecff2f80e982adf4d749d44&json={faceDetectionsPeriod:"+ ofToString(faceDetectionsPeriod) +",faceShotPeriod:" + ofToString(faceShotPeriod)+" }");
             ofLogVerbose() << resp.data << endl;
+            faceShotPeriod,faceDetectionsPeriod,frameRateSum,frameRateCount,frameRateMean=0;
         }
             
     
@@ -41,7 +47,8 @@ public:
         std::string s= "faces in period: " + ofToString(faceDetectionsPeriod) +
                         " ___ Shot in period: " + ofToString(faceShotPeriod)+
                         " ___ Total Faces: " + ofToString(faceDetectionsTotal)+
-                        " ___ Total Shot: " + ofToString(faceShotTotal);
+                        " ___ Total Shot: " + ofToString(faceShotTotal)+
+                                " ___ Framerate: " + ofToString(frameRateMean);
         return s;
     };
     
@@ -50,6 +57,7 @@ public:
         faceShotPeriod=0;
         faceDetectionsTotal=0;
         faceShotTotal=0;
+        
     }
     
 private:
@@ -59,6 +67,9 @@ private:
     int faceShotPeriod=0;
     int faceDetectionsTotal=0;
     int faceShotTotal=0;
+    float frameRateSum=0;
+    float frameRateCount=0;
+    float frameRateMean=0;
     ofFile logTotalFile;
     
     
